79b98d78e1e99b036be287d5e02bd6eb0a560522
Jeppe Richardt <marci@marci.dk>
2015-09-08 21:01:43 +0200

    a little more webapp

--- webservice/app.py
+++ webservice/app.py
@@ -1,4 +1,45 @@
-# To be run by a WSGI server (like gunicorn)
+#gunicorn app:wsgi
 import falcon
+import json
+import models
 
-api = application = falcon.API()
+class ActionsResource(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200
+        query = actions.select().where(actions.userid == 10)
+		for pet in query:
+	     	print pet.id, pet.ip
+
+        resp.body = 'Hello world!'
+ 
+    def on_post(self, req, resp):
+        """Handles POST requests"""
+        try:
+            raw_json = req.stream.read()
+        except Exception as ex:
+            raise falcon.HTTPError(falcon.HTTP_400,
+                'Error',
+                ex.message)
+ 
+        try:
+            result_json = json.loads(raw_json, encoding='utf-8')
+        except ValueError:
+            raise falcon.HTTPError(falcon.HTTP_400,
+                'Malformed JSON',
+                'Could not decode the request body. The '
+                'JSON was incorrect.')
+ 
+        resp.status = falcon.HTTP_202
+        resp.body = json.dumps(result_json, encoding='utf-8')
+ 
+
+
+# falcon.API instances are callable WSGI apps
+wsgi = api = falcon.API()
+ 
+# Resources are represented by long-lived class instances
+actions = ActionsResource()
+ 
+# things will handle all requests to the '/things' URL path
+api.add_route('/actions', things)
